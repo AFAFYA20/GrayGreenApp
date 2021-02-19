fc193a45de255516d1eeab0e219e416dc380c458
AfafAlsamel <68753006+AFAFYA20@users.noreply.github.com>
2021-02-19 12:24:45 +0300

    Update timeline.dart

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:graygreen/widgets/header.dart';
 import 'package:graygreen/widgets/progress.dart';
+
+//create refrence point to collection
+final usersRef = Firestore.instance.collection('Users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +12,36 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  //fetching data from firestore
+  void initState() {
+    getUsers();
+    //getUserById();
+
+    super.initState();
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef.getDocuments();
+    //getDocument is function to get data of each collection
+    //usersRef.getDocuments().then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+        print(doc.documentID);
+        print(doc.exists);
+      });
+   // });
+  }
+  /* getUserById() async{
+   final String id = "3b9xAlnUDz9cXSzOCDKj";
+   final DocumentSnapshot doc = await usersRef.document(id).get();
+     print(doc.data);
+     print(doc.documentID);
+     print(doc.exists);  
+  
+ }
+ */
+
   @override
   Widget build(context) {
     return Scaffold(

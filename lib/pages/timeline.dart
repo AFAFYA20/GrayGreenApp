600b361ced884c8692fbef6e4b858650029f8e50
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-02-20 11:47:44 +0300

    Update timeline.dart

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -14,66 +14,11 @@ class Timeline extends StatefulWidget {
 class _TimelineState extends State<Timeline> {
   @override
   //fetching data from firestore
-  void initState() {
-    //getUserById();
-    //createUser();
-    //updateUser();
-    deletUeser();
-    super.initState();
-  }
-
-  createUser() {
-    usersRef
-        .document("asdfasfd")
-        .setData({"username": "sara", "postcount": 0, "isAdmin": "false"});
-  }
-
-  updateUser() async {
-    final doc = await usersRef.document("3b9xAlnUDz9cXSzOCDKj").get();
-    //To make sure it's exists
-    if (doc.exists) {
-      doc.reference
-          .updateData({"username": "noor", "postcount": 0, "isAdmin": "false"});
-    }
-  }
-
-  deletUeser() async {
-    final DocumentSnapshot doc =
-        await usersRef.document("3b9xAlnUDz9cXSzOCDKj").get();
-    if (doc.exists) {
-      doc.reference.delete();
-    }
-  }
-  /* getUserById() async{
-   final String id = "3b9xAlnUDz9cXSzOCDKj";
-   final DocumentSnapshot doc = await usersRef.document(id).get();
-     print(doc.data);
-     print(doc.documentID);
-     print(doc.exists);  
-  
- }
- */
-
-  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: usersRef.snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return circularProgress();
-          }
-          final List<Text> children = snapshot.data.documents
-              .map((doc) => Text(doc['username']))
-              .toList();
-          return Container(
-            child: ListView(
-              children: children,
-            ),
-          );
-        },
-      ),
-    );
+      body: Text('Timeline'),
+            );
   }
 }
+  

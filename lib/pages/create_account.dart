84095695ce8e5d93a377c9a80d4b353cbc74f864
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-02-20 11:46:26 +0300

    Update create_account.dart

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:graygreen/widgets/header.dart';
+
 
 class CreateAccount extends StatefulWidget {
   @override
@@ -6,8 +8,75 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  final _formKey = GlobalKey<FormState>();
+  String username;
+
+  submit() {
+    _formKey.currentState.save();
+    Navigator.pop(context, username);
+  }
+
   @override
   Widget build(BuildContext parentContext) {
-    return Text("Create Account");
+    return Scaffold(
+      appBar: header(context, titleText: "Set up your profile"),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 25.0),
+                  child: Center(
+                    child: Text(
+                      "Create a userame",
+                      style: TextStyle(fontSize: 20.0),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                    child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        onSaved: (val) => username = val,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "username",
+                          labelStyle: TextStyle(fontSize: 15.0),
+                          hintText: "Must be at least 3 characters",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: submit,
+                  child: Container(
+                    height: 50.0,
+                    width: 350.0,
+                    decoration: BoxDecoration(
+                      color: Colors.green[400],
+                      borderRadius: BorderRadius.circular(7.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

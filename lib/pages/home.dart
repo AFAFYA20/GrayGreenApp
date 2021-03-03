31d0ff4a9ca2bb219cf0b5e760ad53a73c1940f8
AfafAlsamel <68753006+AFAFYA20@users.noreply.github.com>
2021-03-03 12:17:45 +0300

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,21 +1,24 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:graygreen/models/user.dart';
 import 'package:graygreen/pages/create_account.dart';
+import 'package:graygreen/pages/entercountry.dart';
 
-
-import '../models/user.dart';
 import 'activity_feed.dart';
 import 'profile.dart';
 import 'search.dart';
 import 'timeline.dart';
 import 'upload.dart';
+import 'create_account.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final usersRef = Firestore.instance.collection('users');
 final DateTime timestamp = DateTime.now();
-User currentUser;
+
 
 class Home extends StatefulWidget {
   @override
@@ -59,12 +62,15 @@ class _HomeState extends State<Home> {
   createUserInfirestore() async {
     //1)check if user exitsts in users collection in database(according to thier id)
     final GoogleSignInAccount user = googleSignIn.currentUser;
-     DocumentSnapshot doc = await usersRef.document(user.id).get();
+    final DocumentSnapshot doc = await usersRef.document(user.id).get();
     //2) if the user doesnt't exitsts, take them to create account page
     if (!doc.exists) {
       final username = await Navigator.push(
-          context, MaterialPageRoute(builder: (context) => CreateAccount()));
+      context, MaterialPageRoute(builder: (context) => CreateAccount())) ;
 
+      final country = await Navigator.push(
+      context, MaterialPageRoute(builder: (context) => Entercountry())) ;
+      
       //3) get username from create account,use it to make new user document in user collection
       usersRef.document(user.id).setData({
         "id": user.id,
@@ -73,13 +79,10 @@ class _HomeState extends State<Home> {
         "email": user.email,
         "disPlayName": user.displayName,
         "bio": "",
-        "timeStamp": timestamp
+        "country": country ,
+        "timeStamp": timestamp,
       });
-      doc = await usersRef.document(user.id).get();
     }
-    currentUser = User.fromDocument(doc);
-    print(currentUser);
-    print(currentUser.username);
   }
 
   @override
@@ -167,7 +170,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          gradient: LinearGradient(
+            gradient: LinearGradient(
           begin: Alignment.topRight,
           end: Alignment.bottomLeft,
           colors: [

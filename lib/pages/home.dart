ea2ead4f770bb01437ed5b7f89f8aad9992eccce
AfafAlsamel <68753006+AFAFYA20@users.noreply.github.com>
2021-03-17 23:44:55 +0300

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,9 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:graygreen/models/user.dart';
 import 'package:graygreen/pages/create_account.dart';
 import 'package:graygreen/pages/entercountry.dart';
+import 'package:provider/provider.dart';
 
+import '../current_user_model.dart';
 import 'activity_feed.dart';
 import 'profile.dart';
 import 'search.dart';
@@ -18,7 +20,7 @@ import 'create_account.dart';
 final GoogleSignIn googleSignIn = GoogleSignIn();
 final usersRef = Firestore.instance.collection('users');
 final DateTime timestamp = DateTime.now();
-User currentUser;
+AppUser currentUser;
 
 
 class Home extends StatefulWidget {
@@ -64,7 +66,8 @@ class _HomeState extends State<Home> {
     //1)check if user exitsts in users collection in database(according to thier id)
     final GoogleSignInAccount user = googleSignIn.currentUser;
     final DocumentSnapshot doc = await usersRef.document(user.id).get();
-    //currentUser=User.fromDocument(doc);
+
+   
     //2) if the user doesnt't exitsts, take them to create account page
     if (!doc.exists) {
       final username = await Navigator.push(
@@ -74,7 +77,7 @@ class _HomeState extends State<Home> {
       context, MaterialPageRoute(builder: (context) => Entercountry())) ;
       
       //3) get username from create account,use it to make new user document in user collection
-      usersRef.document(user.id).setData({
+     await  usersRef.document(user.id).setData({
         "id": user.id,
         "username": username,
         "photoUrl": user.photoUrl,
@@ -85,7 +88,12 @@ class _HomeState extends State<Home> {
         "timeStamp": timestamp,
       });
 
+
     }
+
+    
+    AppUser currentUser = AppUser.fromDocument(doc);
+     context.read<CurrentUser>().user = currentUser;
   }
 
   @override
@@ -94,8 +102,8 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
-  login() {
-    googleSignIn.signIn();
+  login() async{
+    await googleSignIn.signIn();
   }
 
   logout() {

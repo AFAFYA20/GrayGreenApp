f406e41c499086e4b644469971c51018ee427c58
AfafAlsamel <68753006+AFAFYA20@users.noreply.github.com>
2021-03-18 09:56:53 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:graygreen/pages/home.dart';
+import 'package:provider/provider.dart';
 
-
-
-
-
+import 'current_user_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +11,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'GrayGreen',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.green[600],
-        accentColor: Colors.lightGreen[900],
+    return ChangeNotifierProvider<CurrentUser>.value(
+      value: CurrentUser(),
+      child: MaterialApp(
+        title: 'GrayGreen',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.green[600],
+          accentColor: Colors.lightGreen[900],
+        ),
+        home: Home(),
       ),
-      home: Home(),
     );
   }
 }

9f03c8afee8d8ebdf021088d5e29421b27df0996
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-03-25 20:29:54 +0300

    Update edit_profile.dart

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,13 +1,217 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:graygreen/models/user.dart';
+import 'package:graygreen/pages/home.dart';
+import 'package:graygreen/widgets/progress.dart';
+
+import 'current_user_model.dart';
 
 class EditProfile extends StatefulWidget {
+  final String currentUserId;
+
+  EditProfile({this.currentUserId});
+
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
+  TextEditingController ProfileNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
+  final _scaffoldGlobalKey = GlobalKey<ScaffoldState>();
+  //showing loding spinner when we fetching the user
+  bool isLoding = false;
+  AppUser user;
+  bool _bioValid = true;
+  bool _profileNameValid = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  getUser() async {
+    setState(() {
+      isLoding = true;
+    });
+    //CHECK THIS
+    DocumentSnapshot doc = await usersRef.document(currentUser.id).get();
+    user = AppUser.fromDocument(doc);
+    //CHECK THIS
+    ProfileNameController.text = user.username;
+    bioController.text = user.bio;
+
+    setState(() {
+      isLoding = false;
+    });
+  }
+
+  Column buildDisplayNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            "Profile Name",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: ProfileNameController,
+          decoration: InputDecoration(
+            hintText: "write name here...",
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            hintStyle: TextStyle(color: Colors.grey),
+            errorText: _profileNameValid ? null : "Profile Name is too short",
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column bulidBioField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text(
+            "Bio",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        TextField(
+          controller: bioController,
+          decoration: InputDecoration(
+            hintText: "write Bio here...",
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            hintStyle: TextStyle(color: Colors.grey),
+            errorText: _bioValid ? null : "Bio is too long",
+          ),
+        ),
+      ],
+    );
+  }
+
+  updateUserData() {
+    setState(() {
+      ProfileNameController.text.trim().length < 3 ||
+              ProfileNameController.text.isEmpty
+          ? _profileNameValid = false
+          : _profileNameValid = true;
+      bioController.text.trim().length > 110
+          ? _bioValid = false
+          : _bioValid = true;
+    });
+    if (_bioValid && _profileNameValid)
+      usersRef.document(currentUser.id).updateData({
+        "username": ProfileNameController.text,
+        "bio": bioController.text,
+      });
+    SnackBar successSnackBar = SnackBar(
+      content: Text("Profile has been updated successfully."),
+    );
+    _scaffoldGlobalKey.currentState.showSnackBar(successSnackBar);
+  }
+
+  logout() async {
+    await googleSignIn.signOut();
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Home()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Edit Profile');
+    return Scaffold(
+      key: _scaffoldGlobalKey,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Edit Profile",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.done, size: 30.0, color: Colors.green),
+          ),
+        ],
+      ),
+      body: isLoding
+          ? circularProgress()
+          : ListView(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 16.0, bottom: 8.0),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage:
+                              CachedNetworkImageProvider(user.photoUrl),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            buildDisplayNameField(),
+                            bulidBioField(),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.only(top: 29.0, left: 50.0, right: 50.0),
+                        child: RaisedButton(
+                          onPressed: updateUserData(),
+                          child: Text(
+                            "Update Profile",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.only(top: 10.0, left: 50.0, right: 50.0),
+                        child: RaisedButton(
+                          color: Colors.red,
+                          onPressed: logout(),
+                          child: Text(
+                            'Logout',
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 14.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+    );
   }
 }

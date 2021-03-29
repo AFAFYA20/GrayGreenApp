ffbec2269817420814f5774072ad675f44113eab
Lama Alrweta <lamaalruwaita18@gmail.com>
2021-03-29 09:50:34 +0300

    Update post.dart

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -2,34 +2,34 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:graygreen/models/user.dart';
-import 'package:graygreen/pages/current_user_model.dart';
 import 'package:graygreen/pages/home.dart';
 import 'package:graygreen/widgets/custom_image.dart';
 import 'package:graygreen/widgets/progress.dart';
+import 'package:provider/provider.dart';
 
 class Post extends StatefulWidget {
   final String postId;
-  final String owner;
+  final String ownerId;
   final String username;
-  final String locaion;
+  final String location;
   final String description;
   final String phonenumber;
-  final String dayToMeet;
-  final String meetStart;
-  final String meetEnd;
+  final String daym;
+  final String stime;
+  final String etime;
   final String mediaUrl;
   final dynamic enroll;
 
   Post({
     this.postId,
-    this.owner,
+    this.ownerId,
     this.username,
-    this.locaion,
+    this.location,
     this.description,
     this.phonenumber,
-    this.dayToMeet,
-    this.meetStart,
-    this.meetEnd,
+    this.daym,
+    this.stime,
+    this.etime,
     this.mediaUrl,
     this.enroll,
   });
@@ -37,25 +37,24 @@ class Post extends StatefulWidget {
   factory Post.fromDocument(DocumentSnapshot doc) {
     return Post(
       postId: doc['postId'],
-      owner: doc['owner '],
+      ownerId: doc['ownerId'],
       username: doc['username'],
-      locaion: doc['locaion'],
+      location: doc['location'],
       description: doc['description'],
       phonenumber: doc['phonenumber'],
-      dayToMeet: doc['dayToMeet'],
-      meetEnd: doc['meetEnd'],
+      daym: doc['daym'],
+      stime: doc['stime'],
+      etime: doc['etime'],
       mediaUrl: doc['mediaUrl'],
       enroll: doc['enroll'],
     );
   }
 
   int getEnrollCount(enroll) {
-    // if no enroll, return 0
     if (enroll == null) {
       return 0;
     }
     int count = 0;
-    // if the key is explicitly set to true, add a enroll
     enroll.values.forEach((val) {
       if (val == true) {
         count += 1;
@@ -67,119 +66,82 @@ class Post extends StatefulWidget {
   @override
   _PostState createState() => _PostState(
         postId: this.postId,
-        owner: this.owner,
+        ownerId: this.ownerId,
         username: this.username,
-        locaion: this.locaion,
+        location: this.location,
         description: this.description,
         phonenumber: this.phonenumber,
-        dayToMeet: this.dayToMeet,
-        meetStart: this.meetStart,
-        meetEnd: this.meetEnd,
+        daym: this.daym,
+        stime: this.stime,
+        etime: this.etime,
         mediaUrl: this.mediaUrl,
         enroll: this.enroll,
-        enrollCount: getEnrollCount(this.enroll),
+        EnrollCount: getEnrollCount(this.enroll),
       );
 }
 
 class _PostState extends State<Post> {
   final String postId;
-  final String owner;
+  final String ownerId;
   final String username;
-  final String locaion;
+  final String location;
   final String description;
   final String phonenumber;
-  final String dayToMeet;
-  final String meetStart;
-  final String meetEnd;
+  final String daym;
+  final String stime;
+  final String etime;
   final String mediaUrl;
-  int enrollCount;
+  int EnrollCount;
   Map enroll;
-  bool isEnroll;
-  bool showButton = false;
-  final String currentOnEnrollUserId = currentUser?.id;
+  bool isLiked;
+  bool showheart = false;
+  // final String currentOnlineUser= currentUser.id ;
 
   _PostState({
     this.postId,
-    this.owner,
+    this.ownerId,
     this.username,
-    this.locaion,
+    this.location,
     this.description,
     this.phonenumber,
-    this.dayToMeet,
-    this.meetStart,
-    this.meetEnd,
+    this.daym,
+    this.stime,
+    this.etime,
     this.mediaUrl,
     this.enroll,
-    this.enrollCount,
+    this.EnrollCount,
   });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: 12.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          buildPostHeader(owner),
-          builPostImage(),
-          buildPostFooter(),
-        ],
-      ),
-    );
-  }
-
-  buildPostHeader(owner) {
+  // String Showlocation = location;
+  buildPostHeader() {
     return FutureBuilder(
-      future: usersRef.document(owner).get(),
-      builder: (context, dataSnapshot) {
-        if (!dataSnapshot.hasData) {
+      future: usersRef.document(ownerId).get(),
+      builder: (context, datasnapshot) {
+        if (!datasnapshot.hasData) {
           return circularProgress();
         }
-        AppUser user = AppUser.fromDocument(dataSnapshot.data);
-        //Post post = Post.fromDocument(dataSnapshot.data);
-        bool isPosterOwner = currentOnEnrollUserId == owner;
+
+        AppUser user = AppUser.fromDocument(datasnapshot.data);
+        //  bool isPostOwner= currentOnlineUser == ownerId;
+
         return ListTile(
           leading: CircleAvatar(
-            backgroundImage:
-             CachedNetworkImageProvider(
-                user.photoUrl
-                ),
+            backgroundImage: CachedNetworkImageProvider(user.photoUrl),
             backgroundColor: Colors.grey,
           ),
           title: GestureDetector(
-            onTap: () => print("Show Profile"),
-            child: Text(
-              //user.username
-              user.username,
-              style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-            ),
+              onTap: () => print('showing profile'),
+              child: Text(user.username,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          subtitle: Text(location),
+          trailing: IconButton(
+            onPressed: () => print('deleting post'),
+            icon: Icon(Icons.more_vert),
           ),
-          subtitle: Text(
-            locaion,
-            style: TextStyle(color: Colors.black),
-          ),
-          
-          trailing: isPosterOwner
-              ? IconButton(
-                  icon: Icon(Icons.more_vert, color: Colors.green),
-                  onPressed: () => print("deleted"),
-                )
-              : Text(""),
         );
       },
-    );
-  }
-
-  builPostImage() {
-    return GestureDetector(
-      onDoubleTap: () => print('enroll post'),
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Image.network(mediaUrl),
-        ],
-      ),
     );
   }
 
@@ -191,22 +153,12 @@ class _PostState extends State<Post> {
           children: <Widget>[
             Padding(padding: EdgeInsets.only(top: 40.0, left: 20.0)),
             GestureDetector(
-              onTap: () => print('Enroall button'),
+              onTap: () => print(' Enroll post'),
               child: Icon(
-                Icons.brightness_1_outlined,
-                color: Colors.green[900],
-                // isEnroll? Icons.brightness_1_outlined: Icons.brightness_1,
-                // size: 28.0,
-                // color: Colors.green[900],
-              ),
-            ),
-            Padding(padding: EdgeInsets.only(right: 20.0)),
-            GestureDetector(
-              onTap: () => print('show comments'),
-              child: Icon(
-                Icons.chat_bubble_outline,
+                Icons.check_box_outline_blank,
+                // isLiked? Icons.add : Icons.check,
                 size: 28.0,
-                color: Colors.white,
+                color: Colors.green,
               ),
             ),
           ],
@@ -215,68 +167,75 @@ class _PostState extends State<Post> {
           children: <Widget>[
             Container(
               margin: EdgeInsets.only(left: 20.0),
+              padding: const EdgeInsets.only(bottom: 8),
               child: Text(
-                //username
-                "$enrollCount enroll",
+                "Volunteer Now $EnrollCount",
                 style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          ],
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(left: 20.0),
-              child: Text(
-                "$username",
-                style: TextStyle(
-                  color: Colors.black,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-            Expanded(
-              child: Text(
-                description,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-             Expanded(
-              child: Text(
-                phonenumber,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-             Expanded(
-              child: Text(
-                dayToMeet,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-             Expanded(
-              child: Text(
-                meetStart,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-             Expanded(
-              child: Text(
-                meetEnd,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-             Expanded(
-              child: Text(
-                locaion,
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
           ],
         ),
+      ],
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, phonenumber),
+          _buildButtonColumn(
+            color,
+            Icons.near_me,
+            location,
+          ),
+          _buildButtonColumn(color, Icons.calendar_today, daym),
+          _buildButtonColumn(color, Icons.timelapse, 'From ' + stime),
+          _buildButtonColumn(color, Icons.timelapse, 'To ' + etime),
+        ],
+      ),
+    );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        buildPostHeader(),
+        Image.network(mediaUrl, width: 400, height: 240, fit: BoxFit.cover),
+        buildPostFooter(),
+        buttonSection,
+        textSection,
       ],
     );
   }
